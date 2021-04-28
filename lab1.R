c7630b3dee74e786632c65ebfe1c09d24e5442f3
RabDou <aps.domoslawski@gmail.com>
2021-04-28 22:46:33 +0200

    PierwszeLaby

---
+++ lab1.R
@@ -0,0 +1,42 @@
+# list of tasks
+
+#zad1
+data(iris)
+help(iris)
+View(iris)
+plot(iris$Sepal.Length, iris$Sepal.Width, pch = 19, col = iris$Species)
+legend('topright',legend = levels(iris$Species), fill = 1:3 )
+help(mean)
+tapply(iris$Sepal.Length, iris$Species,mean)
+
+#zad2
+#stworzenie wzorca.rmd
+
+#Zad3
+iris$Sepal.Area = iris$Sepal.Width * iris$Sepal.Length
+
+#zad4
+vec = 1:1000
+vec = vec*2
+vec[ 5*(1:200)] = 0
+vec = c(c(-2, -1), vec, c(2001,2002))
+
+#Zad5
+#wykresy ggplot
+ggplot(iris) + geom_point(aes(x = Sepal.Length, y = Sepal.Width, col = Species))
+# lub
+# ggplot() + geom_point(aes(x = Sepal.Length, y = Sepal.Width, col = Species), data = iris)
+
+
+#Zad 5,5
+n = nrow(DNase)
+df = DNase[1:n *2,]
+arg = aes(x = conc, y = density, col = Run)
+ggplot(df) + geom_line(arg) + geom_point(arg)
+
+# zadania dodatkowe
+#ZAD 1
+X <- data.frame(rnorm(200,4,2))
+d = c (dnorm(x,4,2), x = )
+ggplot(X) + geom_histogram(aes(x = X,y = density)) + geom_line(aes(...), data =)
+
